8511c944a518ef449a095e23e0d6413528e2cb05
Nestor <nestorastheo@gmail.com>
2026-06-08 22:37:44 +0300

    Add backend logging

--- quartet_backend/settings.py
+++ quartet_backend/settings.py
@@ -63,7 +63,19 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
 
 
 CORS_ALLOWED_ORIGINS = [
